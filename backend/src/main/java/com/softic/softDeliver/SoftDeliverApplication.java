ac72fa386a1d0493a0e18741253b723c89c02d21
DanielChombo <danielchombo.dotordaniel@gmail.com>
2023-10-22 23:04:06 +0100

    project criate

---
+++ backend/src/main/java/com/softic/softDeliver/SoftDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.softic.softDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDeliverApplication.class, args);
+	}
+
+}
